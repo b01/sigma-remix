36eda0d4bb9e53ae83a10c5c9245b077c9391587
Khalifah K. Shabazz <shabazzk@gmail.com>
2015-11-28 08:09:56 -0500

    Add a few important things like license text and some code for it to cover.

---
+++ src/Processor.php
@@ -0,0 +1,17 @@
+<?php namespace Kshabazz\Web\SigmaRemix;
+
+/**
+ * Class Processor
+ *
+ * @package \Kshabazz\Web\SigmaRemix
+ */
+class Processor
+{
+	/**
+	 * Processor constructor.
+	 */
+	public function __construct()
+	{
+	}
+}
+?>
