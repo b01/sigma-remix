e69482facdc02b8e4d06d01c80e97635075552f6
Khalifah K. Shabazz <shabazzk@gmail.com>
2015-12-06 09:00:15 -0500

    Add missing ParserException class.

---
+++ src/ParserException.php
@@ -0,0 +1,17 @@
+<?php namespace Kshabazz\SigmaRemix;
+
+/**
+ * Class ParserException
+ *
+ * @package Kshabazz\SigmaRemix
+ */
+class ParserException extends SigmaRemixException
+{
+	const
+		BAD_INCLUDE = 1;
+
+	protected $messages = [
+		self::BAD_INCLUDE => 'Could not INCLUDE template "%s"; Please check that the file exists.'
+	];
+}
+?>
